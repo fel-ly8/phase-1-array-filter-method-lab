28224e2db9f0fc6d17672b5b0a65ac5e9253b0ad
felister kiragu <kiragufelister18@gmail.com>
2024-10-03 20:26:47 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+const drivers = [
+  { name: 'Bobby' },
+  { name: 'Sammy' },
+  { name: 'Sally' },
+  { name: 'Annette' },
+  { name: 'Sarah' }
+];
+
+// findMatching
+console.log(findMatching(drivers.map(d => d.name), 'Sally')); // ['Sally']
+console.log(findMatching(drivers.map(d => d.name), 'sally')); // ['Sally']
+
+// fuzzyMatch
+console.log(fuzzyMatch(drivers.map(d => d.name), 'Sa')); // ['Sammy', 'Sally', 'Sarah']
+console.log(fuzzyMatch(drivers.map(d => d.name), 'll')); // []
+
+// matchName
+console.log(matchName(drivers, 'Sally')); // [{ name: 'Sally' }]
